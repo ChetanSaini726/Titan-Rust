ff149304c449d69aba0c76bbe1dea29f34e8e2e3
ChetanSaini726 <chetansky98@gmail.com>
2022-01-14 19:12:58 +0530

    Change Renderer from 'OpenGl' to 'Vulkan' using vulkano rust crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-extern crate gl;
 extern crate sdl2;
+extern crate vulkano;
 
-pub mod render_gl;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::video::VkInstance;
+use std::ffi::CString;
+use vulkano::instance::{Instance, InstanceExtensions};
+use vulkano::swapchain::Surface;
+use vulkano::{Handle, Version, VulkanObject};
 
 fn main() {
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
     let display_res = 0;
     let width = video_subsystem.display_mode(0, display_res).unwrap().w;
     let height = video_subsystem.display_mode(0, display_res).unwrap().h;
@@ -15,129 +21,38 @@ fn main() {
         .refresh_rate;
     println!("{}x{}x{}", width, height, hz);
 
-    let gl_attr = video_subsystem.gl_attr();
-
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(4, 6);
-
-    let window = video_subsystem
-        .window("Project H", width as u32, height as u32)
-        .opengl()
-        .allow_highdpi()
-        //.resizable()
+    let window = video_subsystem.window("Titan Engine", width as u32, height as u32)
+        .vulkan()
         .build()
         .unwrap();
 
-    let _gl_context = window.gl_create_context().unwrap();
-    let _gl =
-        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
+    let instance_extensions_strings: Vec<CString> = window
+        .vulkan_instance_extensions()
+        .unwrap()
+        .iter()
+        .map(|&v| CString::new(v).unwrap())
+        .collect();
+    let instance_extension =
+        InstanceExtensions::from(instance_extensions_strings.iter().map(AsRef::as_ref));
+    let instance = Instance::new(None, Version::V1_2, &instance_extension, None).unwrap();
+    let surface_handle = window
+        .vulkan_create_surface(instance.internal_object().as_raw() as VkInstance)
+        .unwrap();
+    let surface = unsafe {
+        Surface::from_raw_surface(instance, Handle::from_raw(surface_handle), window.context())
+    };
 
-    // set up shader program
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    use std::ffi::CString;
-    let vert_shader =
-        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert")).unwrap())
-            .unwrap();
-
-    let frag_shader =
-        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag")).unwrap())
-            .unwrap();
-
-    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader]).unwrap();
-
-    // set up vertex buffer object
-
-    let vertices: Vec<f32> = vec![
-        // positions      // colors
-        0.5, -0.5, 0.0, 1.0, 0.0, 0.0, // bottom right
-        -0.5, -0.5, 0.0, 0.0, 1.0, 0.0, // bottom left
-        0.0, 0.5, 0.0, 0.0, 0.0, 1.0, // top
-    ];
-
-    let mut vbo: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenBuffers(1, &mut vbo);
-    }
-
-    unsafe {
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,                                                       // target
-            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
-            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
-            gl::STATIC_DRAW,                               // usage
-        );
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-    }
-
-    // set up vertex array object
-
-    let mut vao: gl::types::GLuint = 0;
-    unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-    }
-
-    unsafe {
-        gl::BindVertexArray(vao);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            0,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            std::ptr::null(),                                     // offset of the first component
-        );
-        gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
-        gl::VertexAttribPointer(
-            1,         // index of the generic vertex attribute ("layout (location = 0)")
-            3,         // the number of components per generic vertex attribute
-            gl::FLOAT, // data type
-            gl::FALSE, // normalized (int-to-float conversion)
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid, // offset of the first component
-        );
-
-        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl::BindVertexArray(0);
-    }
-
-    // set up shared state for window
-
-    unsafe {
-        gl::Viewport(0, 0, width, height);
-        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
-    }
-
-    // main loop
-
-    let mut event_pump = sdl.event_pump().unwrap();
-    'main: loop {
-        for event in event_pump.poll_iter() {
+    'running: loop {
+         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit { .. } => break 'main,
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
                 _ => {}
             }
         }
-
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        // draw triangle
-
-        shader_program.set_used();
-        unsafe {
-            gl::BindVertexArray(vao);
-            gl::DrawArrays(
-                gl::TRIANGLES, // mode
-                0,             // starting index in the enabled arrays
-                3,             // number of indices to be rendered
-            );
-        }
-
-        window.gl_swap_window();
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
